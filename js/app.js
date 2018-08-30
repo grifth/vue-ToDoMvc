8b70d78dc532617e9093e60e255d51a508fda9a8
grifth <2473713213@qq.com>
2018-08-30 09:49:28 +0800

    ver0.1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,42 @@
-(function (window) {
-	'use strict';
+;(function (Vue) {
+	const todos = [
+		{
+			id:1,
+			title:'士郎',
+			completed:false
+		},
+		{
+			id:2,
+			title:'切嗣',
+			completed:false
+		},
+	]
 
-	// Your starting point. Enjoy the ride!
+	new Vue({
+		el:'#todoapp',
+		data:{
+			todos,
+		},
+		methods:{
+			addTodo(){
+				var todoText = event.target.value.trim()
+				if(!todoText.length){
+					return 
+				}
+				var id = this.todos[this.todos.length-1].id+1
 
-})(window);
+				this.todos.push({
+					id,
+					title:todoText,
+					completed:false
+				})
+				event.target.value=''
+			},
+			toggleAll(){
+				var checked = event.target.checked
+				this.todos.forEach(todo=>todo.completed = checked)
+			}
+		}
+	})
+
+})(Vue)
